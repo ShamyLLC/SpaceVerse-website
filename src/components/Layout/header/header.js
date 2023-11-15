f5bbf637d8091c5d6c3a413725437a9611e99d29
rahulpandey <rahul.pandey@vaionex.com>
2023-11-15 11:00:34 +0530

    navbar corrected

--- src/components/Layout/header/header.js
+++ src/components/Layout/header/header.js
@@ -1,43 +1,43 @@
-'use client'
+"use client";
 
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
-import Image from 'next/image'
-import headerbackground from 'src/Images/Header.jpg'
-import vector from 'src/Images/Vector.svg'
-import frame2 from 'src/Images/Frame2.png'
-import clippath from 'src/Images/clippath.svg'
-import arrowright from 'src/Images/arrow-right.svg'
-import homelogo from 'src/Images/Home.svg'
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { PlusIcon } from '@heroicons/react/20/solid'
-import { useImageContext } from '@/contexts/Imagecontext/imageContext'
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import headerbackground from "src/Images/Header.jpg";
+import vector from "src/Images/Vector.svg";
+import frame2 from "src/Images/Frame2.png";
+import clippath from "src/Images/clippath.svg";
+import arrowright from "src/Images/arrow-right.svg";
+import homelogo from "src/Images/Home.svg";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { PlusIcon } from "@heroicons/react/20/solid";
+import { useImageContext } from "@/contexts/Imagecontext/imageContext";
 
 export function Header() {
-  const router = useRouter()
-  const [basePath, setbasePath] = useState('')
-  const { image, changeImage } = useImageContext()
+  const router = useRouter();
+  const [basePath, setbasePath] = useState("");
+  const { image, changeImage } = useImageContext();
 
   const handleChangeImageClick = (newImage) => {
-    changeImage(newImage)
-  }
+    changeImage(newImage);
+  };
 
   const handlewaitlist = () => {
-    router.push('/waitlist')
-  }
+    router.push("/waitlist");
+  };
 
-  const [activeLink, setActiveLink] = useState(null)
+  const [activeLink, setActiveLink] = useState(null);
 
   useEffect(() => {
-    setbasePath(window.location.origin)
-  }, [])
+    setbasePath(window.location.origin);
+  }, []);
 
   return (
     <header
       className="relative"
-      style={{ position: 'relative', width: '100%' }}
+      style={{ position: "relative", width: "100%" }}
     >
       <Image
         src={headerbackground}
@@ -70,23 +70,26 @@ export function Header() {
                   <div className="hidden sm:flex sm:space-x-8">
                     <a
                       href={`${basePath}#home`}
-                      onClick={() => handleChangeImageClick(homelogo)}
+                      onClick={() => {
+                        handleChangeImageClick(homelogo), setActiveLink("home");
+                      }}
                       className={`text-base font-medium  cursor-pointer leading-normal ${
-                        activeLink === 'home'
-                          ? 'text-red-600 underline'
-                          : 'text-white'
+                        activeLink === "home"
+                          ? "text-red-600 underline"
+                          : "text-white"
                       }`}
                     >
                       Home
                     </a>
                     <a
                       onClick={() => {
-                        handleChangeImageClick(homelogo), setActiveLink('about')
+                        handleChangeImageClick(homelogo),
+                          setActiveLink("about");
                       }}
                       className={`text-base cursor-pointer font-medium leading-normal ${
-                        activeLink === 'about'
-                          ? 'text-red-600 underline'
-                          : 'text-white'
+                        activeLink === "about"
+                          ? "text-red-600 underline"
+                          : "text-white"
                       }`}
                     >
                       About us
@@ -94,12 +97,12 @@ export function Header() {
                     <a
                       onClick={() => {
                         handleChangeImageClick(frame2),
-                          setActiveLink('services')
+                          setActiveLink("services");
                       }}
                       className={`text-base cursor-pointer font-medium leading-normal ${
-                        activeLink === 'services'
-                          ? 'text-red-600 underline'
-                          : 'text-white'
+                        activeLink === "services"
+                          ? "text-red-600 underline"
+                          : "text-white"
                       }`}
                     >
                       Our Services
@@ -139,46 +142,46 @@ export function Header() {
 
             <Disclosure.Panel
               className="sm:hidden z-20 "
-              style={{ display: 'block', visibility: '20%' }}
+              style={{ display: "block", visibility: "20%" }}
             >
               <div className="space-y-1 px-2 pb-3 pt-2 z-20 opacity-[99%]">
-                <Disclosure.Button
+                <a
                   href={`${basePath}#home`}
                   onClick={() => {
-                    handleChangeImageClick(homelogo), setActiveLink('home')
+                    handleChangeImageClick(homelogo), setActiveLink("home");
                   }}
                   className={`text-base font-medium leading-normal  block rounded-md bg-transparent px-3 py-2 z-20 text-white  ${
-                    activeLink === 'home'
-                      ? 'text-red-600 underline'
-                      : 'text-white'
+                    activeLink === "home"
+                      ? "text-red-600 underline"
+                      : "text-white"
                   }`}
                 >
                   Home
-                </Disclosure.Button>
-                <Disclosure.Button
+                </a>
+                <a
                   onClick={() => {
-                    handleChangeImageClick(homelogo), setActiveLink('about')
+                    handleChangeImageClick(homelogo), setActiveLink("about");
                   }}
                   className={`text-base cursor-pointer font-medium leading-normal  block rounded-md bg-transparent px-3 py-2  ${
-                    activeLink === 'about'
-                      ? 'text-red-600 underline'
-                      : 'text-white'
+                    activeLink === "about"
+                      ? "text-red-600 underline"
+                      : "text-white"
                   }`}
                 >
                   About us
-                </Disclosure.Button>
-                <Disclosure.Button
+                </a>
+                <a
                   onClick={() => {
-                    handleChangeImageClick(frame2), setActiveLink('services')
+                    handleChangeImageClick(frame2), setActiveLink("services");
                   }}
                   className={`text-base cursor-pointer font-medium leading-normal  block rounded-md bg-transparent px-3 py-2  text-white z-20 ${
-                    activeLink === 'services'
-                      ? 'text-red-600 underline'
-                      : 'text-white'
+                    activeLink === "services"
+                      ? "text-red-600 underline"
+                      : "text-white"
                   }`}
                 >
                   Our Services
-                </Disclosure.Button>
+                </a>
               </div>
               <div className="border-t-2 ml-2 border-white pb-3 pt-4 z-20 opacity-[99%] ">
                 <button className="bg=transparent rounded-md shadow border border-white justify-center items-center inline-flex p-2.5 my-2 sm:my-0">
@@ -196,5 +199,5 @@ export function Header() {
         )}
       </Disclosure>
     </header>
-  )
+  );
 }
